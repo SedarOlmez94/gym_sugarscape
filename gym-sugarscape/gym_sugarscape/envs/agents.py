e2d714f116bdcbd07455b88252140dfa1650acbd
Sedar Olmez <solmez@turing.ac.uk>
2019-11-18 13:42:33 +0000

    Sugar is assigned to the 2D array, agent objects are also assigned
    - The agents appear at random locations on the grid.

--- gym-sugarscape/gym_sugarscape/envs/agents.py
+++ gym-sugarscape/gym_sugarscape/envs/agents.py
@@ -3,12 +3,13 @@ random.seed(9001)
 
 class Agent:
 
-    def __init__(self):
+    def __init__(self, ID):
         self.vision = random.randrange(1, 6)
         self.metabolic_rate = random.randrange(1, 4)
         self.age = random.randrange(0, 100)
         self.s_wealth = random.randrange(5, 25)
         self.sugar_collected = 0
+        self.ID = ID
         self.visual = "X"
 
     def get_vision(self):
@@ -32,5 +33,5 @@ class Agent:
     def collect_sugar(self, environment_cell_sugar):
         self.sugar_collected = environment_cell_sugar
 
-X = Agent()
-print(X.get_visual())
+    def get_ID(self):
+        return self.ID
--- gym-sugarscape/gym_sugarscape/envs/sugarscape_env.py
+++ gym-sugarscape/gym_sugarscape/envs/sugarscape_env.py
@@ -5,6 +5,8 @@ import logging
 import numpy
 import sys
 import random
+from agents import Agent
+
 numpy.set_printoptions(threshold=sys.maxsize)
 logger = logging.getLogger(__name__)
 
@@ -58,15 +60,33 @@ class SugarscapeEnv(gym.Env):
     def reset(self):
         # Set of initialised variables for each agent.
         self.growth_rate = 1
-        self.environment = numpy.arange(2500).reshape(50, 50)
-        self.environment.fill(0)
-        self.agents = 250
+        self.environment = numpy.empty((50,50), dtype=numpy.object)
+        #self.environment.fill(0)
+        list_of_agents = []
+        number_of_agents = 0
+        test_loop = 0
+
+        for i in range(250):
+            list_of_agents.append(Agent(i))
 
         for i in range(50):
             for j in range(50):
                 self.environment[i, j] = random.randrange(0, 4)
 
-        return self._next_observation()
+        while(number_of_agents != 250):
+            x = random.randrange(50)
+            y = random.randrange(50)
+            if(self.environment[x, y] == 0):
+                self.environment[x, y] = list_of_agents[number_of_agents].get_visual()
+                number_of_agents = number_of_agents + 1
+
+        # for i in range(50):
+        #     for j in range(50):
+        #         if(self.environment[i, j] == "X"):
+        #             test_loop = test_loop + 1
+
+        print(self.environment)
+
 
     def render(self, mode = 'human'):
         # Render the environment to the screen.
@@ -87,3 +107,4 @@ class SugarscapeEnv(gym.Env):
             return 0
 
 x = SugarscapeEnv()
+x.reset()
