e6ef1f381f3383946bb7ce42a4410d43fb2abc46
Sedar Olmez <solmez@turing.ac.uk>
2019-11-25 12:17:13 +0000

    Fixing unit test API travis.

--- gym-sugarscape/gym_sugarscape/envs/testing_code.py
+++ gym-sugarscape/gym_sugarscape/envs/testing_code.py
@@ -2,9 +2,6 @@ from sugarscape_env import SugarscapeEnv
 from agents import Agent
 import math
 
-import pytest
-pytest.main()
-
 
 def test_get_vision():
     agent_1 = Agent(1)
@@ -14,6 +11,8 @@ def test_get_vision():
 def test__get_status():
     environment_test = SugarscapeEnv()
     environment_test.reset(10, 50)
-    environment_test.step('N')
-
     assert environment_test._get_status() == 'SOME AGENTS STILL ALIVE'
+
+
+import pytest
+pytest.main()
